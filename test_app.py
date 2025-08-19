48addef8635a7d7b2a75c449adf9689ddc3bacd0
Akash <akashtelvekar97@gmail.com>
2025-08-19 08:29:00 +0530

    docker cicd yaml file is ready

--- test_app.py
+++ test_app.py
@@ -1,2 +1,12 @@
 # all unit test will be here
 
+from app import app
+
+
+def test_home():
+      response = app.test_client().get('/')
+
+      assert response.status_code==200
+      assert response.data==b'Hello World!'
+
+      
